f39ea44a9908cbc09b06ca8ac7097c8ed04bdc1e
Murillo Ramos <murillo.tech.dev@gmail.com>
2025-10-30 22:36:02 -0300

    Day 1

--- html-ts-coffee-cu/index-js.js
+++ html-ts-coffee-cu/index-js.js
@@ -1,5 +1,20 @@
-console.log('hi');
-console.log('hi');
-console.log('hi');
-console.log('hi');
-console.log('hi');
+import { createWalletClient, custom } from "https://esm.sh/viem"
+
+const connectButton = document.getElementById("connectButton")
+
+let walletClient
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum) // In the future we can put here different kinds of transports, which means more wallet compatibility within our application
+        })
+        await walletClient.requestAddresses()
+        connectButton.innerHTML = "Connected!"
+
+    } else {
+        connectButton.innerHTML = "Please install the MetaMask Application!"
+    }
+}
+
+connectButton.onclick = connect
