546ae4cea50c54df8129625e386ec5d1ffe186b6
Murillo Ramos <murillo.tech.dev@gmail.com>
2025-10-31 22:35:24 -0300

    Day 2

--- html-ts-coffee-cu/index-js.js
+++ html-ts-coffee-cu/index-js.js
@@ -1,8 +1,12 @@
-import { createWalletClient, custom } from "https://esm.sh/viem"
+import { createWalletClient, custom, createPublicClient, parseEther, defineChain } from "https://esm.sh/viem"
+import { contractAddress, abi } from "./constants-js.js"
 
 const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+const ethAmountInput = document.getElementById("ethAmount")
 
 let walletClient
+let publicClient
 
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
@@ -17,4 +21,51 @@ async function connect() {
     }
 }
 
+async function fund() {
+    const ethAmount = ethAmountInput.value
+    console.log(`Funding with ${ethAmount}...`)
+
+    if (typeof window.ethereum !== "undefined") {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        })
+        const [connectedAccount] = await walletClient.requestAddresses()
+        const currentChain = await getCurrentChain(walletClient)
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        })
+        await publicClient.simulateContract({
+            address: contractAddress,
+            abi: abi,
+            functionName: "fund",
+            account: connectedAccount,
+            chain: currentChain,
+            value: parseEther(ethAmount),
+        })
+    } else {
+        connectButton.innerHTML = "Please install the MetaMask Application!"
+    }
+
+}
+
+async function getCurrentChain(client) {
+    const chainId = await client.getChainId()
+    const currentChain = defineChain({
+        id: chainId,
+        name: "Custom Chain",
+        nativeCurrency: {
+            name: "Ether",
+            symbol: "ETH",
+            decimals: 18,
+        },
+        rpcUrls: {
+            default: {
+                http: ["http://localhost:8545"],
+            },
+        },
+    })
+    return currentChain
+}
+
 connectButton.onclick = connect
+fundButton.onclick = fund
