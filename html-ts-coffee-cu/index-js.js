5d1d7d20cb8ce0dccaa213353018d78fb22f8ba1
Murillo Ramos <murillo.tech.dev@gmail.com>
2025-11-03 23:03:46 -0300

    Day 4

--- html-ts-coffee-cu/index-js.js
+++ html-ts-coffee-cu/index-js.js
@@ -1,75 +1,91 @@
-import { createWalletClient, custom, createPublicClient, parseEther, defineChain } from "https://esm.sh/viem"
+import { createWalletClient, custom, createPublicClient, parseEther, defineChain, formatEther } from "https://esm.sh/viem"
 import { contractAddress, abi } from "./constants-js.js"
 
-const connectButton = document.getElementById("connectButton")
-const fundButton = document.getElementById("fundButton")
-const ethAmountInput = document.getElementById("ethAmount")
+document.addEventListener('DOMContentLoaded', function () {
+    // This says: "Wait until ALL HTML is ready, THEN run this code"
+    const connectButton = document.getElementById("connectButton")
+    const fundButton = document.getElementById("fundButton")
+    const ethAmountInput = document.getElementById("ethAmount")
+    const balanceButton = document.getElementById("balanceButton")
 
-let walletClient
-let publicClient
+    let walletClient
+    let publicClient
 
-async function connect() {
-    if (typeof window.ethereum !== "undefined") {
-        walletClient = createWalletClient({
-            transport: custom(window.ethereum) // In the future we can put here different kinds of transports, which means more wallet compatibility within our application
-        })
-        await walletClient.requestAddresses()
-        connectButton.innerHTML = "Connected!"
+    async function connect() {
+        if (typeof window.ethereum !== "undefined") {
+            walletClient = createWalletClient({
+                transport: custom(window.ethereum) // In the future we can put here different kinds of transports, which means more wallet compatibility within our application
+            })
+            await walletClient.requestAddresses()
+            connectButton.innerHTML = "Connected!"
 
-    } else {
-        connectButton.innerHTML = "Please install the MetaMask Application!"
-    }
-}
-
-async function fund() {
-    const ethAmount = ethAmountInput.value
-    console.log(`Funding with ${ethAmount}...`)
-
-    if (typeof window.ethereum !== "undefined") {
-        walletClient = createWalletClient({
-            transport: custom(window.ethereum)
-        })
-        const [connectedAccount] = await walletClient.requestAddresses()
-        const currentChain = await getCurrentChain(walletClient)
-        publicClient = createPublicClient({
-            transport: custom(window.ethereum)
-        })
-        const { request } = await publicClient.simulateContract({
-            address: contractAddress,
-            abi: abi,
-            functionName: "fund",
-            account: connectedAccount,
-            chain: currentChain,
-            value: parseEther(ethAmount),
-        })
-
-        const hash = await walletClient.writeContract(request)
-        console.log(hash)
-
-    } else {
-        connectButton.innerHTML = "Please install the MetaMask Application!"
+        } else {
+            connectButton.innerHTML = "Please install the MetaMask Application!"
+        }
     }
 
-}
+    async function fund() {
+        const ethAmount = ethAmountInput.value
+        console.log(`Funding with ${ethAmount}...`)
 
-async function getCurrentChain(client) {
-    const chainId = await client.getChainId()
-    const currentChain = defineChain({
-        id: chainId,
-        name: "Custom Chain",
-        nativeCurrency: {
-            name: "Ether",
-            symbol: "ETH",
-            decimals: 18,
-        },
-        rpcUrls: {
-            default: {
-                http: ["http://localhost:8545"],
+        if (typeof window.ethereum !== "undefined") {
+            walletClient = createWalletClient({
+                transport: custom(window.ethereum)
+            })
+            const [connectedAccount] = await walletClient.requestAddresses()
+            const currentChain = await getCurrentChain(walletClient)
+            publicClient = createPublicClient({
+                transport: custom(window.ethereum)
+            })
+            const { request } = await publicClient.simulateContract({
+                address: contractAddress,
+                abi: abi,
+                functionName: "fund",
+                account: connectedAccount,
+                chain: currentChain,
+                value: parseEther(ethAmount), // 1 -> 100000000000000000
+            })
+
+            const hash = await walletClient.writeContract(request)
+            console.log(hash)
+
+        } else {
+            connectButton.innerHTML = "Please install the MetaMask Application!"
+        }
+
+    }
+
+    async function getCurrentChain(client) {
+        const chainId = await client.getChainId()
+        const currentChain = defineChain({
+            id: chainId,
+            name: "Custom Chain",
+            nativeCurrency: {
+                name: "Ether",
+                symbol: "ETH",
+                decimals: 18,
             },
-        },
-    })
-    return currentChain
-}
+            rpcUrls: {
+                default: {
+                    http: ["http://localhost:8545"],
+                },
+            },
+        })
+        return currentChain
+    }
 
-connectButton.onclick = connect
-fundButton.onclick = fund
+    async function getbalance() {
+        if (typeof window.ethereum !== "undefined") {
+            publicClient = createPublicClient({
+                transport: custom(window.ethereum)
+            })
+            const balance = await publicClient.getBalance({
+                address: contractAddress
+            })
+            console.log(formatEther(balance)) // 1000000000000000000 -> 1
+        }
+    }
+    connectButton.onclick = connect
+    fundButton.onclick = fund
+    balanceButton.onclick = getbalance
+})
